76e74a3daad598d274c9f5faab90e3f28f0f1388
Anouar Naouri <102242949+AnouarVision@users.noreply.github.com>
2025-12-15 21:55:59 +0100

    fix: resolve SCSS variables usage and incorrect component imports

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import HomeHero from './HomeHero';
+import HomeTopics from './HomeTopics';
+
+export default function Home(){
+    return(
+        <>
+            <Navbar />
+            <HomeHero />
+            <HomeTopics />
+            <Footer />
+        </>
+    )
+}
